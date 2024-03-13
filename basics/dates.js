2485ab09ba2c0d3dd8328778613979a823398533
shreykosti <158189084+shreykosti@users.noreply.github.com>
2024-03-13 06:32:57 +0000

    commit dATE

---
+++ basics/dates.js
@@ -0,0 +1,9 @@
+let mydata = new Date();
+
+console.log(mydata.toString());
+console.log(mydata.toDateString());
+console.log(mydata.toLocaleString());
+
+let myCreatDate = new Date(2023 , 0, 24,10,23)
+//input year,month(starts from zero),day,hrs min
+console.log(myCreatDate.toLocaleString());
