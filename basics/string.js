8fd0936f991dad816fe99e492641509803f9b7a2
shreykosti <158189084+shreykosti@users.noreply.github.com>
2024-03-13 05:02:08 +0000

    changes in test  and string

---
+++ basics/string.js
@@ -0,0 +1,8 @@
+let name = "shrey"
+let repoCount = "49"
+
+//modern way of concatinating string// 
+console.log(`Hellow my name is ${name} and my repo couny is ${repoCount}`);
+
+//defining string 
+let heapstring = new String("shrey");
