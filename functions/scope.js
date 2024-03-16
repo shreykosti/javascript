bf90dc79d82548911dcebe12d048a710cf40986a
shreykosti <158189084+shreykosti@users.noreply.github.com>
2024-03-16 22:21:15 +0000

    commit

---
+++ functions/scope.js
@@ -0,0 +1,24 @@
+//global scope
+let a = 10;
+const b = 20;
+var c = 30;//scope problem
+
+if(true){
+    //block scope
+
+}
+
+//defining function 
+
+function addone(num){
+
+}
+
+const addtwo = function(num){//expression
+        //uper necha call nahi car sakta//
+        //sirf nech(declare karna ka bad kar sakta ha)//
+};
+
+
+
+
