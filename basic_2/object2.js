7adb8d4a0c2255d86ae6b3485ef1c0f80721e8be
shreykosti <158189084+shreykosti@users.noreply.github.com>
2024-03-16 08:46:18 +0000

    comit obj2

---
+++ basic_2/object2.js
@@ -0,0 +1,10 @@
+const human = {
+    name : "shrey",
+    age : 20,
+    gender : "male",
+    girlfriend : 1,
+}
+
+//accesing value in object(de-structure)
+const {age : shreyage} = human;
+console.log(shreyage);
