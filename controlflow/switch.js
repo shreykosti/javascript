38ae758cb6ebd3552c2c0d4b3f4093ee387876b5
shreykosti <158189084+shreykosti@users.noreply.github.com>
2024-03-17 16:20:56 +0000

    commit

---
+++ controlflow/switch.js
@@ -0,0 +1,18 @@
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+// Nullish Coalescing Operator (??) : null undeffined
+
+const val1 = null??10;
+
+//returns value other then null or undefined 
+
+//terniary operator
+
+//conndition ? true : false
